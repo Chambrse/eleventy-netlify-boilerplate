4743c4b9265d67d685548658b67117c038e57c52
Shane Chambry <Shaneechambry@gmail.com>
2020-07-28 07:21:52 -0400

    more template conversions

--- .eleventy.js
+++ .eleventy.js
@@ -90,7 +90,7 @@ module.exports = function(eleventyConfig) {
   );
 
   return {
-    templateFormats: ["md", "njk", "html", "liquid"],
+    templateFormats: ["md", "njk", "html", "liquid", "jpg", "png", "svg"],
 
     // If your site lives in a different subdirectory, change this.
     // Leading or trailing slashes are all normalized away, so don’t worry about it.
