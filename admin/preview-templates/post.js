d1828888a43a407b697267da09e0fa06b8c9da31
Shane Chambry <Shaneechambry@gmail.com>
2020-07-28 15:36:43 -0400

    fix subtitles

--- admin/preview-templates/post.js
+++ admin/preview-templates/post.js
@@ -8,28 +8,40 @@ const Post = createClass({
   render() {
     const entry = this.props.entry;
 
-    return (
-    <header class="masthead">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-                <div class="site-heading">
-                    <h1>{entry.getIn(["data", "title"], "title")}</h1>
-                    <span class="subheading">{entry.getIn(["data", "subtitle"], "")}</span>
-                        <p class="post-meta">Posted by
-                    <a href='/authors/{{ author | slug }}'> {entry.getIn(["data", "author"], "")} </a>
-                    on {
-                      format(
-                        entry.getIn(["data", "date"], new Date()),
-                        "dd MMM, yyyy"
-                      )
-                      }</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>)
+    return html`
+      <main>
+        <article>
+          <h1>${entry.getIn(["data", "title"], null)}</h1>
+          <p>
+            <small>
+              <time
+                >${
+      format(
+        entry.getIn(["data", "date"], new Date()),
+        "dd MMM, yyyy"
+      )
+      }</time
+              >
+              ${" by Author"}
+            </small>
+          </p>
+
+          <p>${entry.getIn(["data", "summary"], "")}</p>
+
+          ${this.props.widgetFor("body")}
+          <p>
+            ${
+      entry.getIn(["data", "tags"], []).map(
+        tag =>
+          html`
+                    <a href="#" rel="tag">${tag}</a>
+                  `
+      )
+      }
+          </p>
+        </article>
+      </main>
+    `;
   }
 });
 
